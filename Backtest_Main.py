992a309559170ae1291429530d72407bc826f08a
Babacar Gueye <82913973+BabacarG@users.noreply.github.com>
2022-02-02 00:23:18 +0100

    Add files via upload

---
+++ Backtest_Main.py
@@ -0,0 +1,23 @@
+from Backtest_SMA_EMA import *
+
+# Paste your own api keys which you can get by creating a Binance account
+api_key = ''
+api_secret_key = ''
+
+# Enter a ticker symbol (ex: ETHUSDT, ETHBTC, SOLUSDT...)
+asset = "BTCUSDT"
+
+# Available timeframes : 1min, 3min, 5min, 15min, 30min, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1m
+timeFrame = "1d"
+
+# Choose a start date for the data (Follow this format: "1 Jan, 2020")
+startDate = "1 Jan, 2015"
+
+backtest1 = CryptoBackTest(api_key, api_secret_key, asset, timeFrame, startDate)
+
+# You can display the raw data graph but a graph will be opened in anyway after a backtest
+# backtest1.plot()
+
+# Choose if you want to use SMA or EMA and enter as parameter the range of SMA/EMA periods you want to compare
+backtest1.buysell1sma(2, 10)
+# backtest1.buysell1ema(2, 10)
